29b796abe283c85b208848ec3b90f25455b63f29
chrisHuila <christian.huila22@gmail.com>
2023-06-30 23:24:12 -0500

    remove tags added

--- app/(registered)/new-product/CurrentTags.tsx
+++ app/(registered)/new-product/CurrentTags.tsx
@@ -5,10 +5,23 @@ interface Props {
 }
 
 const CurrentTags = ({ tags, settags: setTags }: Props) => {
+    
+    const removetag = (id: string) => {
+        const newTags = tags.filter(tag => tag.id !== id);
+        setTags(newTags)
+    }
+
     return (
         <ul className="tags-all show-tags">
             {tags?.map(tag => (
-                <li key={tag.id}>{tag.tag}</li>
+                <li key={tag.id}>
+                    {tag.tag}
+                    <button
+                    className="remove-item"
+                    onClick={() => removetag(tag.id)}>
+                    &times;
+                    </button>
+                </li>
             ))}
         </ul>
       );
