ca434c064efd8a6ed8afa824660188a3ae5ec6dc
chrisHuila <christian.huila22@gmail.com>
2023-07-05 19:16:02 -0500

    auth with module css

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/navigation'
 import firebase from "@/firebase/firebase";
 import useValidation from "@/hooks/useValidation";
 import signupValidation from "@/services/validation/signupValidation";
+import style from "@/app/(auth)/Auth.module.css";
 
 const initialState = {
     user_name: '',
@@ -37,11 +38,11 @@ const SignUp = () => {
 
     return (
          <main className="minvh">
-            <div className="user_form">
-                <div className="form-container">
-                    <h2 className="title-login">Sign up</h2>
+            <div className={style.user_form}>
+                <div className={style.form_container}>
+                    <h2 className={style.title_login}>Sign up</h2>
                     <form action="" onSubmit={onSubmit}>
-                        <div className="form-field">
+                        <div className={style.form_field}>
                             <label htmlFor="user_name">Name</label>
                             <input 
                             type="text"
@@ -54,7 +55,7 @@ const SignUp = () => {
                         </div>
                         {errors.user_name && <p className="auth-error"> {errors.user_name}</p>}
 
-                        <div className="form-field">
+                        <div className={style.form_field}>
                             <label htmlFor="email">E-mail</label>
                             <input 
                             type="email"
@@ -67,7 +68,7 @@ const SignUp = () => {
                         </div>
                         {errors.email && <p className="auth-error"> {errors.email}</p>}
 
-                        <div className="form-field">
+                        <div className={style.form_field}>
                             <label htmlFor="password">Password</label>
                             <input 
                             type="password"
@@ -80,7 +81,7 @@ const SignUp = () => {
                         </div>
                         {errors.password && <p className="auth-error"> {errors.password}</p>}
 
-                        <div className="form-field">
+                        <div className={style.form_field}>
                             <label htmlFor="password">Confirm your Password</label>
                             <input 
                             type="password"
@@ -93,15 +94,15 @@ const SignUp = () => {
                         </div>
                         {errors.confirm && <p className="auth-error"> {errors.confirm}</p>}
                         
-                        <div className="form-field">
+                        <div className={style.form_field}>
                             <input 
                             type="submit"
                             value='Sign up'
-                            className="btn btn-primary btn-block"
+                            className={`${style.btn} ${style.btn_primary} ${style.btn_block}`}
                              />
                         </div>
                     </form>
-                    <Link href="/login" className="account-link">
+                    <Link href="/login" className={style.account_link}>
                         Log in
                     </Link>
                     {errorauth && <p className="auth-error"> {errorauth}</p>}
