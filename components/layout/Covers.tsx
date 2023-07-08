2ac5540321c0d3f0719025788a9cd3272c3511bc
chrisHuila <christian.huila22@gmail.com>
2023-07-08 03:06:36 -0500

    cover with text in english

--- components/layout/Covers.tsx
+++ components/layout/Covers.tsx
@@ -2,13 +2,13 @@ import style from "@/components/layout/Covers.module.css"
 
 const Covers = ({cover }: {cover: string }) => {
     // Retorna el mes actual
-    let mesActual = new Intl.DateTimeFormat('es-ES', { month: 'long'}).format(new Date());
+    let currentMonth = new Intl.DateTimeFormat('en-En', { month: 'long'}).format(new Date());
 
     return (
         <div className={style.cover} style={{backgroundImage: `url(${cover})`}}>
            <div className={style.cover_container}>
                 <div className={`container ${style.cover_main}`}>
-                    <h1 className={style.cover_title}>Las mejores ofertas de {mesActual}<span>Descuebre lo que tenemos para ti</span></h1>
+                    <h1 className={style.cover_title}>The best {currentMonth} sales <span>Discover what we have for you</span></h1>
                 </div>
             </div> 
         </div>
